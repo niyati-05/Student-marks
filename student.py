4c4078fe72db5e5aa418c36e4cbae23f36581918
niyati05 <niyatigogri05@gmail.com>
2025-11-19 09:50:15 +0530

    save

---
+++ student.py
@@ -0,0 +1,16 @@
+import sys
+
+if len(sys.argv)== 4:
+    system_name = sys.argv[0]
+    student_name = sys.argv[1]
+    marks1 = sys.argv[2]
+    marks2 = sys.argv[3]
+
+else:
+    system_name = sys.argv[0]
+    student_name = "Niyati"
+    marks1 = "18" 
+    marks2 = "20"
+
+    average_score = (int(marks1) + int(marks2)) / 2
+    print(f"Average internal score is {average_score}")
